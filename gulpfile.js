49c474df42201f7ca282facac7cf8c2182aaabd4
HAROUNA MAYAOU <hm.nassirou@gmail.com>
2017-10-27 18:04:04 +0200

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,27 @@
-/**
- * Created by nharouna on 26/10/17.
- */
-gulp.task('css', function(){
-   return gulp.src()
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+gulp.task('sass', function() {
+   console.log('herue');
+
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+    }))
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function() {
+   gulp.watch('app/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('browserSync', function() {
+  browserSync({
+      server: {
+          baseDir: 'app'
+      }
+  }
+  )
 });
